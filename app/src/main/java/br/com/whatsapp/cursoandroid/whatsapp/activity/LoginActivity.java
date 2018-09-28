cd5af4eb082b945394fa0483e2b8f05be2baf6cf
Jefferson de Deus Leite <jd.leite@hotmail.com>
2018-09-27 22:47:14 -0300

    Tela de login e validação do SMS feita, porém sem os códigos e validações.A tela de login está meio torta pois o Android Studio está repleto de bugs (programa 100% atualizado).

---
+++ app/src/main/java/br/com/whatsapp/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package br.com.whatsapp.cursoandroid.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.whatsapp.cursoandroid.whatsapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
