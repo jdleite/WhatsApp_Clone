1956df55d8dceb8b14e4ce30a49688ec43a06dd1
Jefferson de Deus Leite <jd.leitee@gmail.com>
2018-10-22 20:20:12 -0300

    Feito a alteração das cores do menu

--- app/src/main/java/br/com/whatsapp/cursoandroid/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/br/com/whatsapp/cursoandroid/whatsapp/activity/LoginActivity.java
@@ -2,17 +2,33 @@ package br.com.whatsapp.cursoandroid.whatsapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.database.DatabaseReference;
 
 import br.com.whatsapp.cursoandroid.whatsapp.R;
 import br.com.whatsapp.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
+import br.com.whatsapp.cursoandroid.whatsapp.model.Usuario;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private DatabaseReference referenciaFirebase;
+    private EditText email;
+    private EditText senha;
+    private Button botaoLogar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +36,82 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+    verificarUsuarioLogado();
+
+
+
+        email = (EditText) findViewById(R.id.edit_login_email);
+        senha = (EditText) findViewById(R.id.edit_login_senha);
+        botaoLogar = (Button) findViewById(R.id.bt_logar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                usuario = new Usuario();
+                usuario.setEmail( email.getText().toString() );
+                usuario.setSenha( senha.getText().toString() );
+                validarLogin();
+
+            }
+        });
 
     }
 
-    public void abrirCadastroUsuario(View view) {
-        Intent i = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
-        startActivity(i);
+    private void verificarUsuarioLogado(){
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        if( autenticacao.getCurrentUser() != null ){
+            abrirTelaPrincipal();
+        }
+    }
+
+    private void validarLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if( task.isSuccessful() ){
+                    abrirTelaPrincipal();
+                    Toast.makeText(LoginActivity.this, "Sucesso ao fazer login!", Toast.LENGTH_LONG ).show();
+                }else{
+
+                    String erroExcecao = "";
+
+                    try {
+                        throw task.getException();
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erroExcecao = "Email ou senha inválidos";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        erroExcecao = "Já existe um usuário logado com esse e-mail";
+                    }catch (Exception e){
+                        erroExcecao = "Erro ao logar";
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this, "Erro: " + erroExcecao, Toast.LENGTH_LONG ).show();
+                }
+            }
+        });
+
+    }
+
+
+    private void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void abrirCadastroUsuario(View view){
+
+        Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+        startActivity( intent );
+
     }
 
 }
--- app/src/main/java/br/com/whatsapp/cursoandroid/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/com/whatsapp/cursoandroid/whatsapp/activity/MainActivity.java
@@ -1,8 +1,16 @@
 package br.com.whatsapp.cursoandroid.whatsapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -10,8 +18,10 @@ import br.com.whatsapp.cursoandroid.whatsapp.R;
 import br.com.whatsapp.cursoandroid.whatsapp.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private Button botaoSair;
+    private FirebaseAuth autenticacao;
+    private android.support.v7.widget.Toolbar toolbar;
+    private FirebaseAuth usuarioAutenticacao;
 
 
     @Override
@@ -19,7 +29,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        usuarioAutenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
 
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_main, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.item_configuracoes:
+                return  true;
+            default:
+                 return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void deslogarUsuario(){
+
+        usuarioAutenticacao.signOut();
+        Intent i = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(i);
+        finish();
 
     }
 }
